171c3a6da9e0f40830ee9ae05eb999c0ea51bc73
PasanSasmika <pasansasmika198@gmail.com>
2026-09-07 10:38:17 +0530

    Detail page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Register from "./pages/register";
 import ProtectedRoute from "./components/protectedRoutes";
 import ReportHistory from "./pages/team/ReportHistory";
 import ReportForm from "./pages/team/ReportForm";
+import ReportDetail from "./pages/team/ReportDetail";
 
 
 function HomeRedirect() {
@@ -24,40 +25,12 @@ export default function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
 
-          <Route
-  path="/my-reports"
-  element={
-    <ProtectedRoute role="team_member">
-      <ReportHistory />
-    </ProtectedRoute>
-  }
-/>
-          <Route
-            path="/manager/dashboard"
-            element={
-              <ProtectedRoute role="manager">
-                <div className="p-8">Manager dashboard (coming in Step 11)</div>
-              </ProtectedRoute>
-            }
-          />
-
-
-          <Route
-  path="/my-reports/new"
-  element={
-    <ProtectedRoute role="team_member">
-      <ReportForm />
-    </ProtectedRoute>
-  }
-/>
-<Route
-  path="/my-reports/:id/edit"
-  element={
-    <ProtectedRoute role="team_member">
-      <ReportForm />
-    </ProtectedRoute>
-  }
-/>
+          <Route path="/my-reports" element={<ProtectedRoute role="team_member"><ReportHistory /></ProtectedRoute>}/>
+          
+          <Route path="/manager/dashboard" element={<ProtectedRoute role="manager"><div className="p-8">Manager dashboard</div></ProtectedRoute>}/>
+          <Route path="/my-reports/new" element={<ProtectedRoute role="team_member"><ReportForm /></ProtectedRoute>}/>
+         <Route path="/my-reports/:id/edit" element={<ProtectedRoute role="team_member"><ReportForm /></ProtectedRoute>}/>
+         <Route path="/my-reports/:id" element={<ProtectedRoute role="team_member"><ReportDetail /></ProtectedRoute>}/>
         </Routes>
       </AuthProvider>
     </BrowserRouter>
